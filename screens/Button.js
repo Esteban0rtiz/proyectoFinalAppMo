d1e27976e604c7866f34c5fdef75e79fd219aa9d
DannyMeneses <dannycrackdeadpool@gmail.com>
2023-08-01 21:22:44 -0500

    Segundo commit creacion ventanas login(esteban), registro(Danny),Buttton(Danny)

---
+++ screens/Button.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Login from './screens/Login';
+import Registro from './screens/Registro';
+import Home from './screens/home';
+const Tab = createBottomTabNavigator();
+
+const Screen1 = () => (
+  <View style={styles.container}>
+    <Text style={styles.title}>Login</Text>
+    <Login></Login>
+  </View>
+);
+
+const Screen2 = () => (
+  <View style={styles.container}>
+    <Text style={styles.title}>Registro</Text>
+    <Registro></Registro>
+  </View>
+);
+
+const Screen3 = () => (
+  <View style={styles.container}>
+    <Text style={styles.title}>Productos</Text>
+    <Productos></Productos>
+  </View>
+);
+const Screen4 = () => (
+  <View style={styles.container}>
+    <Text style={styles.title}>Home</Text>
+    <Home></Home>
+  </View>
+);
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === 'Inicio') {
+              iconName = 'ios-home';
+            } else if (route.name === 'perfil') {
+              iconName = 'ios-list';
+            } else if (route.name === 'productos') {
+              iconName = 'ios-person';
+            }else if (route.name === 'transacciones') {
+              iconName = 'ios-person';
+            }
+
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#007bff',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Login" component={Screen1} />
+        <Tab.Screen name="Registro" component={Screen2} />
+        <Tab.Screen name="Productos" component={Screen3} />
+        <Tab.Screen name="Home" component={Screen4} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f2f2f2',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+});
+
+export default App;
---
+++ screens/Registro.js
@@ -0,0 +1,115 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const App = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleRegister = () => {
+    if (name.trim() === '' || email.trim() === '' || password.trim() === '') {
+      setErrorMessage('Por favor, completa todos los campos.');
+    } else if (!email.includes('@')) {
+      setErrorMessage('El correo electrónico debe contener el símbolo "@".');
+    } else {
+      console.log('Nombre:', name);
+      console.log('Correo:', email);
+      console.log('Contraseña:', password);
+      setErrorMessage('');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Icon class="fi fi-sr-edit"></Icon>
+      <Text style={styles.title}>Registro</Text>
+      <View style={styles.inputContainer}>
+        <Icon name="person-outline" size={20} color="#ff0000" style={styles.icon} />
+        <TextInput
+          style={styles.input}
+          placeholder="Nombre"
+          value={name}
+          onChangeText={setName}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Icon name="mail-outline" size={20} color="#ff0000" style={styles.icon} />
+        <TextInput
+          style={styles.input}
+          placeholder="Correo electrónico"
+          keyboardType="email-address"
+          value={email}
+          onChangeText={setEmail}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Icon name="lock-closed-outline" size={20} color="#ff0000" style={styles.icon} />
+        <TextInput
+          style={styles.input}
+          placeholder="Contraseña"
+          secureTextEntry
+          value={password}
+          onChangeText={setPassword}
+        />
+      </View>
+      {errorMessage ? <Text style={styles.errorMessage}>{errorMessage}</Text> : null}
+      <TouchableOpacity style={styles.button} onPress={handleRegister}>
+        <Text style={styles.buttonText}>Registrarse</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    color: '#ff0000'
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '80%',
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
+    color: '#fff'
+
+  },
+  button: {
+    backgroundColor: '#ff0000',
+    padding: 10,
+    borderRadius: 5,
+    width: '80%',
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 18,
+  },
+  errorMessage: {
+    color: '#ff0000',
+    fontSize: 16,
+    marginBottom: 10,
+  },
+});
+
+export default App;
+
